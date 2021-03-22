cb0431e1e4b43a1773b9f9261831f78c6a769af6
Gustavo Riquena <gustavo_riquena1@outlook.com>
2021-03-22 20:29:33 -0300

    removing long methods and simplifying structure

--- src/main/java/rpa/challenge/selenium/controller/ChallengeController.java
+++ src/main/java/rpa/challenge/selenium/controller/ChallengeController.java
@@ -3,7 +3,6 @@ package rpa.challenge.selenium.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.math3.analysis.function.Log;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -22,17 +21,10 @@ public class ChallengeController {
 		personList = excelController.readRowsExcel();
 		
 		driver = new ChromeDriver();
-		openRpaChallenge();
-		inputData();
-	}
-	
-	private void openRpaChallenge() {
-		driver.get("http://www.rpachallenge.com/");
-	}
-	
-	private void inputData() {
+		
 		for (Person person : personList) {
 			try {
+				driver.get("http://www.rpachallenge.com/");
 				driver.findElement(By.xpath("//div//label[contains(text(), 'First Name')]//following-sibling::input")).sendKeys(person.getFirstName());
 				driver.findElement(By.xpath("//div//label[contains(text(), 'Last Name')]//following-sibling::input")).sendKeys(person.getLastName());
 				driver.findElement(By.xpath("//div//label[contains(text(), 'Role')]//following-sibling::input")).sendKeys(person.getRoleInCompany());
@@ -42,15 +34,11 @@ public class ChallengeController {
 				driver.findElement(By.xpath("//div//label[contains(text(), 'Phone')]//following-sibling::input")).sendKeys(person.getPhoneNumber());
 				driver.findElement(By.xpath("//form//input[@Type='submit' or contains(text(), 'submit') or starts-with(@class, 'btn')]")).click();
 			} catch (Exception e) {
-				driver.findElement(By.xpath("//div//label[contains(text(), 'First Name')]//following-sibling::input")).clear();
-				driver.findElement(By.xpath("//div//label[contains(text(), 'Last Name')]//following-sibling::input")).clear();
-				driver.findElement(By.xpath("//div//label[contains(text(), 'Role')]//following-sibling::input")).clear();
-				driver.findElement(By.xpath("//div//label[contains(text(), 'Company Name')]//following-sibling::input")).clear();
-				driver.findElement(By.xpath("//div//label[contains(text(), 'Address')]//following-sibling::input")).clear();
-				driver.findElement(By.xpath("//div//label[contains(text(), 'Email')]//following-sibling::input")).clear();
-				driver.findElement(By.xpath("//div//label[contains(text(), 'Phone')]//following-sibling::input")).clear();
 				log.error(e.getMessage());
 			}
 		}
+		
+		driver.close();
+		driver.quit();
 	}
 }
